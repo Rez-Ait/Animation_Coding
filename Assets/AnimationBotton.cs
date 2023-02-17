a467dc40952e2304bfc714b1bd1c8f4fdf149d72
Rez-Ait <61746389+Rez-Ait@users.noreply.github.com>
2023-02-17 23:55:20 +0330

    Complet V1

--- Assets/AnimationBotton.cs
+++ Assets/AnimationBotton.cs
@@ -1,201 +1,214 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class AnimationBotton : MonoBehaviour
 {
+    [field: SerializeField, Range(0, 10)]
+    private float delayStart;
     [field: SerializeField]
-    public List<BtnAction> lsBtnAction = new List<BtnAction>();
+    private List<BtnAction> btnAction = new List<BtnAction>(0);
+
+    Vector3? startScale, startPosition, startRotation;
+
     [field: SerializeField]
-    public bool isRepit = true;
+    private bool isAutoStart = true;
+    [field: SerializeField]
+    private bool isRepet = true;
+
+    //private Coroutine _coroutine;
 
 
-    private RectTransform myRectTransform;
-    //private Rect myRect;
-
-    //public bool isStartDelay;
-    //[Range(1, 10)]
-    //public float startDelay = 1;
-    //public bool isDelayForAllStep;
-    //[Range(1, 10)]
-    //public float delayForAllStep = 1;
-    //[field: SerializeField]
-    //public bool isRepit = true;
-
-    void Start()
+    public void AddAction(BtnAction action)
     {
-        //myRect.GetComponent<Rect>();
-        myRectTransform = GetComponent<RectTransform>();
-        StartCoroutine(Animat());
-
+        btnAction.Add(action);
+    }
+    public void AddAction(List<BtnAction> actions)
+    {
+        btnAction.AddRange(actions);
     }
 
-    //float Y = 0;
+    private void OnEnable()
+    {
+        Debug.Log("Enable Object", this);
+        if (!isAutoStart)
+            return;
+
+        startPosition ??= transform.localPosition;
+        startRotation ??= transform.localEulerAngles;
+        startScale ??= transform.localScale;
+
+        if (!Vector3.Equals(transform.localPosition, startPosition))
+            this.transform.localPosition = (Vector3)startPosition;
+
+        if (!Vector3.Equals(transform.localRotation, startRotation))
+            this.transform.localRotation = Quaternion.Euler((Vector3)startRotation);
+
+        if (!Vector3.Equals(transform.localScale, startScale))
+            this.transform.localScale = (Vector3)startScale;
+
+        StartCoroutine(Animat());
+
+        //_coroutine = StartCoroutine(Animat());
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Disable Object", this);
+        if (!isAutoStart)
+            return;
+
+        //if (_coroutine != null) 
+        //  StopCoroutine(_coroutine);
+    }
+
 
     IEnumerator Animat()
     {
-        int step = 0;
-        int maxStep = lsBtnAction.Count;
-        while (true)
-        {
-            yield return new WaitForSeconds(1f);
+        bool tmpIsRepit = true;
+        int stepAction = 0;
+        float currentTime = 0, endTime;
+        ActionType currentAction;
+        BtnAction myAction;
+        Vector3 originalScale;
+        Vector3 originalPosition;
+        Vector3 endState;
+        Quaternion originalRotation;
+        Quaternion endStateRotation;
+        Action<bool> func;
+        yield return new WaitForSeconds(delayStart);
 
-            BtnAction btnAction = lsBtnAction[step];
-            float defaltTime = Time.realtimeSinceStartup + btnAction.timeAnimation;
-            switch (btnAction.actionType)
+        if (btnAction.Count != 0)
+            while (tmpIsRepit)
             {
-                case ActionType.Rotation:
 
-                    while (true)
+                for (int i = 0; i < btnAction.Count; i++)
+                {
+
+                    originalPosition = transform.localPosition;
+                    originalRotation = transform.localRotation;
+                    originalScale = transform.localScale;
+                    myAction = btnAction[stepAction];
+                    endStateRotation = Quaternion.Euler((Vector3)(myAction.endState + startRotation));
+                    endTime = myAction.timeAnimation;
+                    currentAction = myAction.actionType;
+                    endState = myAction.endState;
+                    func = null;
+                    //Debug.Log($"Start Action : {currentAction}");
+
+                    switch (currentAction)
                     {
-                        Rotation(btnAction.speed, false);
-
-
-                        yield return new WaitForSeconds(0.01f);
-
-                        if (Time.realtimeSinceStartup >= defaltTime && !btnAction.isBackStep)
-                            break;
-                        else
-                        {
-                            defaltTime = Time.realtimeSinceStartup + btnAction.timeAnimation;
-                            while (true)
+                        case ActionType.Position:
+                            func = (bool isBack) =>
                             {
-                                Rotation(btnAction.speed, true);
-                                yield return new WaitForSeconds(0.01f);
+                                if (!isBack)
+                                    transform.localPosition = Vector2.Lerp(originalPosition, (Vector3)(startPosition + endState), currentTime / endTime);
+                                else
+                                    transform.localPosition = Vector2.Lerp((Vector3)(startPosition + endState), originalPosition, currentTime / endTime);
+                            };
+                            break;
 
-                                if (Time.realtimeSinceStartup >= defaltTime)
-                                {
+                        case ActionType.Rotation:
+                            func = (bool isBack) =>
+                            {
+                                if (!isBack)
+                                    transform.localRotation = Quaternion.Lerp(originalRotation, endStateRotation, currentTime / endTime);
+                                else
+                                    transform.localRotation = Quaternion.Lerp(endStateRotation, originalRotation, currentTime / endTime);
+                            };
+                            break;
 
-                                }
-                                break;
+                        case ActionType.Scale:
+                            func = (bool isBack) =>
+                            {
+                                if (!isBack)
+                                    transform.localScale = Vector2.Lerp(originalScale, (Vector3)(startScale + myAction.endState), currentTime / endTime);
+                                else
+                                    transform.localScale = Vector2.Lerp((Vector3)(startScale + myAction.endState), originalScale, currentTime / endTime);
+                            };
+                            break;
 
-                            }
-                        }
-                    }
-                    break;
-                case ActionType.Scale:
-
-                    while (true)
-                    {
-                        Scale(btnAction.speed);
-
-                        yield return new WaitForSeconds(0.01f);
-
-                        if (Time.realtimeSinceStartup >= defaltTime)
+                        default:
                             break;
                     }
+                    Transform tmp = transform;
+                    if (currentAction != ActionType.none)
+                    {
+                        currentTime = 0;
+                        do
+                        {
+                            func(false);
+                            currentTime += Time.deltaTime;
+                            yield return null;
+                        } while (currentTime <= endTime);
+                        currentTime = 0;
+                        if (myAction.isBackStep)
+                            do
+                            {
+                                func(true);
+                                currentTime += Time.deltaTime;
+                                yield return null;
+                            } while (currentTime <= endTime);
 
-                    break;
-                case ActionType.Position:
+                        transform.localPosition = (Vector3)startPosition;
+                        transform.localRotation = Quaternion.Euler((Vector3)startRotation);
+                        transform.localScale = (Vector3)startScale;
+                    }
+                    else
+                        yield return new WaitForSeconds(endTime * 2);
 
-                    //while (true)
-                    //{
-                    //    Scale(btnAction.speed);
 
-                    //    yield return new WaitForSeconds(0.01f);
+                    stepAction++;
+                    if (stepAction % btnAction.Count == 0)
+                        stepAction = 0;
+                }
 
-                    //    if (Time.realtimeSinceStartup >= defaltTime)
-                    //        break;
-                    //}
+                tmpIsRepit = isRepet;
 
-                    break;
-                default:
-                    break;
             }
 
-            if (!isRepit)
-                break;
-
-            step++;
-            if (step >= maxStep)
-                step = 0;
-
-        }
-
     }
 
-    public void Rotation(float speed, bool isBack)
-    {
-        if (isBack)
-        {
-
-            myRectTransform.transform.Rotate(0, 0, -(speed * Time.deltaTime));
-        }
-        else
-        {
-            myRectTransform.transform.Rotate(0, 0, speed * Time.deltaTime);
-
-        }
-        //myRectTransform.transform.Rotate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
-        //this.transform.rotation = new Quaternion(0, 1, 0, 0);
-    }
-
-    //public void Position(ref float step)
-    //{
-    //    myRectTransform.rect.Set(0, step += Time.deltaTime, 0, 0);
-    //    //this.transform.position = new Vector3(0, 0, 1);
-    //}
-
-    public void Scale(float speed)
-    {
-        myRectTransform.localScale += new Vector3(1f, 1f) * speed * Time.deltaTime;
-        //this.transform.localScale = new Vector3(1, 1);
-    }
 }
 
+//switch (currentAction)
+//{
+//    case ActionType.Position:
+//        transform.localPosition = Vector2.Lerp(this.transform.position, myAction.endState, currentTime / endTime);
+//        break;
+//    case ActionType.Rotation:
+//        transform.Rotate(myAction.endState * currentTime / endTime);
+//        break;
+//    case ActionType.Scale:
+//        transform.localScale = Vector2.Lerp(this.transform.position, myAction.endState, currentTime / endTime);
+//        break;
+//    default:
+//        break;
+//}
 
 [Serializable]
 public class BtnAction
 {
     public ActionType actionType;
-    [Range(1, 500)]
-    public float speed;
-    public float timeAnimation;
-    public bool isBackStep = true;
+    public float timeAnimation = 1;
+    public bool isBackStep = false;
+    public Vector3 endState;
+
+    public BtnAction() { }
+    public BtnAction(ActionType _actionType, float _timeAnimation, bool _isBackStep, Vector3 _endState)
+    {
+        timeAnimation = _timeAnimation;
+        isBackStep = _isBackStep;
+        actionType = _actionType;
+        endState = _endState;
+    }
 }
-
-//[Serializable]
-//public class ButtonActions : ButtonAction
-//{
-
-
-//    [field: SerializeField, Range(0.1f, 50)]
-//    private float timeAnimation = 1;
-
-//    public ButtonActions() { }
-//    public ButtonActions(AnimationType _animationType, float _speed, bool _isDelay, float _delay, float _timeAnimation, bool _isRepit)
-//    {
-//        animationType = _animationType;
-//        speed = _speed;
-//        isDelay = _isDelay;
-//        delay = _delay;
-//        timeAnimation = _timeAnimation;
-//    }
-//}
-
-//public class ButtonAction
-//{
-//    [field: SerializeField]
-//    private ActionType actionType = ActionType.Scale;
-//    [field: SerializeField, Range(1, 100)]
-//    private float speed = 10;
-//    [field: SerializeField]
-//    private bool isDelay = false;
-//    [field: SerializeField, Range(1, 10)]
-//    private float delay = 1;
-//    [field: SerializeField]
-//    private bool isBack = true;
-//}
-
 
 public enum ActionType
 {
+    none,
+    Position,
     Rotation,
-    Scale,
-    Position
+    Scale
 }
